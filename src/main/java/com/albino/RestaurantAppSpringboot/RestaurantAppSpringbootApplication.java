aaa6e06d8f9932d53a3f43d5fc4608b9c44484d2
Albino Braganza <albinobraganza21@gmail.com>
2025-03-15 10:58:30 +0530

    Initail git project setup check-in

---
+++ src/main/java/com/albino/RestaurantAppSpringboot/RestaurantAppSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.albino.RestaurantAppSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantAppSpringbootApplication.class, args);
+		System.out.println(	"Hola Mundo");
+	}
+
+}
---
+++ src/test/java/com/albino/RestaurantAppSpringboot/RestaurantAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albino.RestaurantAppSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
